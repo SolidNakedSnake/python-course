da9eaac6a50f5e303ddb23521940f48e008cc513
SolidNakedSnake <vs.solidsnake@gmail.com>
2026-07-02 12:02:39 +0200

    fix: text typo

--- basics/01_io_variables/main.py
+++ basics/01_io_variables/main.py
@@ -1,16 +1,16 @@
 # Temporary script for manual dataset entry
 # TODO: Connect a real database later when I learn how to do it
 
-print("=== DATASET INGESTIOM SYSTEM ===", end="\n\n")
+print("=== DATASET INGESTION SYSTEM ===", end="\n\n")
 
-# Codllecting features )variables and inputs)
+# Collecting features (variables and inputs)
 sample_id = input("Enter sample ID (e.g., 001): ")
 text_data = input("Enter text string for analysis: ")
 data_label = input("Enter label (Positive / Negative): ")
 
-print()  # Just a blank line for clean terminasl output
+print()  # Just a blank line for clean terminals output
 
-# Testing output format with  sep and end parameters
+# Testing output format with sep and end parameters
 print("Saved samples", sample_id, sep=": ")
 print("Raw Text Data", text_data, sep="->")
 
